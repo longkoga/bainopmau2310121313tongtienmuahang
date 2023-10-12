012e360b7093d34bf10e84ce3511ad6f85f2b715
longkoga <147029431+longkoga@users.noreply.github.com>
2023-10-12 07:07:20 +0000

    nop bai

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,8 @@ tongtienmuahang(None)                   | None
 
 #region bailam
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+  if gia_truoc_thue==None:
+    return(None)
+  else:
+    return round(gia_truoc_thue*1.1)
 #endregion bailam
